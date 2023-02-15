4e78e8bfe049009874d4904a9b884bfdd4ae4470
David Heitmeyer <david@heitmeyer.org>
2023-02-14 20:27:00 -0500

    starting point

--- src/_data/prizes.js
+++ src/_data/prizes.js
@@ -1,26 +1,19 @@
 const eleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function () {
-  /* The Nobel Prize
+  /* The Nobel Prize API
      https://www.nobelprize.org/about/developer-zone-2/  */
 
   let baseUrl = "https://api.nobelprize.org/2.1/nobelPrizes";
-  let userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:102.0) Gecko/20100101 Firefox/102.0';
-
+  
   try {
-    console.log(baseUrl);
     let prizesData = await eleventyFetch(baseUrl, {
-      fetchOptions: {
-        headers: {
-          "User-Agent": userAgent
-        }
-      },
       duration: "1d",
-      type: "json",
+      type: "json"
     });
     return(prizesData);
   } catch (err) {
-    console.error("something is wrong");
+    console.error("Something went wrong :( ");
     console.log(err);
   }
 };

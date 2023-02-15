c4521fefc5983347a36fba8e384f594a74f8f696
David Heitmeyer <david@heitmeyer.org>
2023-02-14 20:04:09 -0500

    start of class

--- src/_data/prizes.js
+++ src/_data/prizes.js
@@ -1,4 +1,4 @@
-const EleventyFetch = require("@11ty/eleventy-fetch");
+const eleventyFetch = require("@11ty/eleventy-fetch");
 
 module.exports = async function () {
   /* The Nobel Prize
@@ -6,34 +6,17 @@ module.exports = async function () {
 
   let baseUrl = "https://api.nobelprize.org/2.1/nobelPrizes";
   let userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:102.0) Gecko/20100101 Firefox/102.0';
-  let sort = "desc";
-  let limit = 1000;
-  let yearStart = 2000;
-  let now = new Date();
-  let yearEnd = now.getFullYear();
-
-  let prizeParams = {
-    sort: sort,
-    limit: limit,
-    nobelPrizeYear: yearEnd,
-    yearTo: yearStart
-  };
 
   try {
-    let params = new URLSearchParams(prizeParams);
-    let queryString = params.toString();
-    let url = baseUrl + "?" + queryString;
-    console.log(url);
-    let prizesData = await EleventyFetch(url, {
+    console.log(baseUrl);
+    let prizesData = await eleventyFetch(baseUrl, {
       fetchOptions: {
         headers: {
-          "User-Agent": userAgent,
-        },
+          "User-Agent": userAgent
+        }
       },
       duration: "1d",
       type: "json",
-    }).then((jsonData) => {
-      return jsonData;
     });
     return(prizesData);
   } catch (err) {

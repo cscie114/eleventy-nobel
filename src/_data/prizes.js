f7b4bfa4b70b48b1c678d3b175a0fa0a098d99ca
David Heitmeyer <david@heitmeyer.org>
2023-02-15 23:03:50 -0500

    end of class on 2/14/2023

--- src/_data/prizes.js
+++ src/_data/prizes.js
@@ -5,9 +5,25 @@ module.exports = async function () {
      https://www.nobelprize.org/about/developer-zone-2/  */
 
   let baseUrl = "https://api.nobelprize.org/2.1/nobelPrizes";
+  /*
+  sort
+  limit
+  nobelPrizeYear (end year)
+  yearTo (start year)
+  */
+  let requestParams = {
+    sort: "desc",
+    limit: 1000
+  }
+  let params = new URLSearchParams(requestParams);
+  let queryString = params.toString();
+  console.log(queryString);
   
+  let requestUrl = baseUrl + "?" + queryString;
+
+  console.log(requestUrl);
   try {
-    let prizesData = await eleventyFetch(baseUrl, {
+    let prizesData = await eleventyFetch(requestUrl, {
       duration: "1d",
       type: "json"
     });

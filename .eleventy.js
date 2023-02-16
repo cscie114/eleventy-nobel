2d057b12155cc9f595b968d4bf20984dfa9cf170
David Heitmeyer <david@heitmeyer.org>
2023-02-16 00:22:18 -0500

    end of bonus segment for render and shortcodes

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,19 @@
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/assets/**");
 
+	eleventyConfig.addShortcode("laureateDisplayName", 
+	function(laureate) {
+		let displayName = '';
+		if (("knownName" in laureate)
+		     && ("en" in laureate.knownName)) {
+			displayName = laureate.knownName.en;
+		} else if (("orgName" in laureate)
+		     && ("en" in laureate.orgName)) {
+			displayName = laureate.orgName.en;
+		}
+		return displayName;
+	});
+
 	return {
     	"dir" : {
         	"input": "src",
